897fc9fed9c1e8d3b47285c8e5a3eca6947bd28a
karthikselvaam <karthikselvaam@gmail.com>
2021-05-14 01:18:10 +0530

    nextgreatelement

---
+++ Stacks/nextgreatestelement.py
@@ -0,0 +1,22 @@
+arr  = [18,7,6,12,15]
+#output = [-1,12,12,15,-1]
+
+def nextGreatestElement(arr):
+    nextgreatelemenlist = [-1 for i in range(len(arr))]
+    stack = []
+    stack.append(0)
+
+    for k in range(1, len(arr)):
+        while len(stack) != 0 and arr[k] >  arr[stack[-1]]:
+            index = stack.pop()
+            nextgreatelemenlist[index] = arr[k]
+
+        if arr[k] <= arr[stack[-1]]:
+            stack.append(k)
+
+    return nextgreatelemenlist
+
+
+
+result = nextGreatestElement(arr)
+print(result)
